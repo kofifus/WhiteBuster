58bb65ed1bd3bfd75ebdfa6ebc2fa395683c2e73
Kofifus <mikagenic@gmail.com>
2017-07-30 18:10:43 +1000

    Update whitebuster.js

--- whitebuster.js
+++ whitebuster.js
@@ -1,98 +1,145 @@
 (function () { 
 
-const hex = "f5f5f5";
 
-const hexRGBArray = hex.match(/[A-Za-z0-9]{2}/g).map(v => parseInt(v, 16));
-const hexRGBAprefix = hexRGBArray[0] + ', ' + hexRGBArray[1] + ', ' + hexRGBArray[2];
+	const hex = "f5f5f5";
 
-function addCSSclass(rules) {
-	const style = document.createElement("style");
+	const hexRGBArray = hex.match(/[A-Za-z0-9]{2}/g).map(v => parseInt(v, 16));
+	const RGBAstr = 'rgba(' + hexRGBArray[0] + ', ' + hexRGBArray[1] + ', ' + hexRGBArray[2];
+	const RGBstr = 'rgb(' + hexRGBArray[0] + ', ' + hexRGBArray[1] + ', ' + hexRGBArray[2] + ')';
 
-	style.appendChild(document.createTextNode("")); // WebKit hack :(
-	document.head.appendChild(style);
-	const sheet = style.sheet;
+	// Returns a function, that, when invoked, will only be triggered at most once
+	// during a given window of time. Normally, the throttled function will run
+	// as much as it can, without ever going more than once per `wait` duration;
+	// but if you'd like to disable the execution on the leading edge, pass
+	// `{leading: false}`. To disable execution on the trailing edge, ditto.
+	function throttle(func, wait, options) {
+		var timeout, context, args, result;
+		var previous = 0;
+		if (!options) options = {};
 
-	rules.forEach((rule, index) => {
+		var later = function () {
+			previous = options.leading === false ? 0 : new Date().getTime();
+			timeout = null;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		};
+
+		var throttled = function () {
+			var now = new Date().getTime();
+			if (!previous && options.leading === false) previous = now;
+			var remaining = wait - (now - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0 || remaining > wait) {
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+				previous = now;
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			} else if (!timeout && options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
+			}
+			return result;
+		};
+
+		throttled.cancel = function () {
+			clearTimeout(timeout);
+			previous = 0;
+			timeout = context = args = null;
+		};
+
+		return throttled;
+	}
+
+	function convertBGstr(str, prefix) {
+		prefix=prefix ? '(background(?:\\-color)?:[ ]?)' : '()';
+		let regRgba = new RegExp(prefix + 'rgba\\((?:255, 255, 255|254, 254, 254)', 'ig');
+		let regRgb = new RegExp(prefix +'(?:white|#fff(?:fff)?|#fefefe|rgb\\(255, 255, 255\\)|rgb\\(254, 254, 254\\))', 'ig');
+	
+		s=str.replace(regRgba, '$1'+RGBAstr);
+		s=s.replace(regRgb, '$1'+RGBstr);
+
+		//if (s!==str) console.log(str + ' => ' + s +'\n');
+		return s;
+	}
+
+	function convertCSS() {
+		for (const styleSheet of window.document.styleSheets) {
+			const classes = styleSheet.rules || styleSheet.cssRules;
+			if (!classes) continue;
+
+			for (const cssRule of classes) {
+				if (cssRule.type !== 1 || !cssRule.style) continue;
+				const selector = cssRule.selectorText, declaration=cssRule.style.cssText;
+				if (!selector || !declaration) continue;
+
+				let s=convertBGstr(declaration, true);
+				if (s !== declaration) cssRule.style.cssText = s;
+			}
+		}
+	}
+
+	function getBGcolor(elem) {
 		try {
-				sheet.insertRule(rule.selector + "{" + rule.rule + "}", index);
+			return window.getComputedStyle(elem, null).getPropertyValue('background-color');
 		} catch (e) {
-				// firefox can break here          
-		}
-	})
-}
-
-// If newState is provided add/remove theClass accordingly, otherwise toggle theClass
-function toggleClass(elem, theClass, newState, first = false) {
-	const matchRegExp = new RegExp('(?:^|\\s)' + theClass + '(?!\\S)', 'g');
-	const add = (arguments.length > 2 ? !!newState : (elem.className.match(matchRegExp) == null));
-
-	elem.className = elem.className.replace(matchRegExp, ''); // clear all
-	if (add) {
-		if (first) elem.className = theClass + ' ' + elem.className;
-		else elem.className += ' ' + theClass;
-	}
-}
-
-function convertColorStr(str) {
-	s=str.replace('rgba(255, 255, 255', 'rgba(' + hexRGBAprefix);
-	s=s.replace('rgb(255, 255, 255)', '#' + hex);
-	s=s.replace('white', '#' + hex);
-	s=s.replace('#FFFFFF', '#' + hex);
-	s=s.replace('#FFF', '#' + hex);
-	s=s.replace('#ffffff', '#' + hex);
-	s=s.replace('#fff', '#' + hex);
-	//if (s!==str) console.log(str + ' => ' + s +'\n');
-	return s;
-}
-
-function convertBody() {
-	addCSSclass([{
-		selector: '.whiteBuster',
-		rule: 'background-color: #' + hex
-	}]);
-	toggleClass(document.body, 'whiteBuster', true, true);
-}
-
-function convertCSS() {
-	for (const styleSheet of window.document.styleSheets) {
-		const classes = styleSheet.rules || styleSheet.cssRules;
-		if (!classes) continue;
-
-		for (const cssRule of classes) {
-			if (cssRule.type !== 1 || !cssRule.style) continue;
-			const r=cssRule.style.cssText;
-			if (!r.startsWith('background: ') && !r.startsWith('background-color: ')) continue;
-			const s=convertColorStr(r);
-			if (s !== r) cssRule.style.cssText = s;
+			return '';
 		}
 	}
-}
 
-function convertElem(elem) {
-	try {
-		const bg = 'background-color: ' + window.getComputedStyle(elem, null).getPropertyValue('background-color');
-		if (convertColorStr(bg) !== bg) elem.style.backgroundColor = '#' + hex;
-	} catch (e) {
-		// swallow error
+	function setBGcolor(elem, color=RGBstr) {
+		try {
+			elem.style.backgroundColor = color;
+		} catch (e) {
+			/* swallow error */
+		}
 	}
-}
 
-function convertAllElems() {
-	for (elem of document.getElementsByTagName("*")) convertElem(elem);
-}
 
-function convertMutated() {
-	const observer = new MutationObserver(mutations => {
+	function convertElem(elem) {
+		if (!elem.style) return;
+		const body=(elem===document.body);
+		let bg = elem.style.backgroundColor;
+		if (!body && (bg === undefined || bg === RGBstr || bg.startsWith(RGBAstr) || bg === 'transparent' || bg.startsWith('rgba(0, 0, 0, 0)'))) {
+			// nothing to do
+		} else if (body || !bg) {
+			bg = getBGcolor(elem, null);
+			if (bg && convertBGstr(bg, false) !== bg) setBGcolor(elem);
+		} else if (convertBGstr(bg, false) !== bg) {
+			setBGcolor(elem);
+		}
+	}
+
+	function convertAllElems(root=document) {
+		if (!root.getElementsByTagName) return;
+
+		for (elem of root.getElementsByTagName('*')) {
+			if (elem.tagName==='IFRAME') {
+				try { convertAllElems(elem.contentDocument); } catch(e) {};
+			} else {
+				convertElem(elem);
+			}
+		}
+	}		
+
+	function handleMutation(mutations) {
 		mutations.forEach(mutation => {
-			for (elem of mutation.addedNodes) convertElem(elem);
+			for (elem of mutation.addedNodes) {
+				convertElem(elem);
+				convertAllElems(elem);
+			}
 		});
-	});
-	observer.observe(document, { attributes: true, childList: true, characterData: true, subtree:true });
-}
+	}
+	const throttledHandleMution = throttle(handleMutation, 200);
+	
+	function convertMutated() {
+		const observer = new MutationObserver(mutations => throttledHandleMution(mutations));
+		observer.observe(document, { childList: true, subtree:true });
+	}
 
-convertBody();
-convertCSS();
-convertAllElems();
-convertMutated();
-
+	convertCSS();
+	convertAllElems();
+	convertMutated();
 })();
