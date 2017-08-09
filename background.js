ac73dada13b17478ddd4d1e784c5bc53cb42e450
Kofifus <mikagenic@gmail.com>
2017-08-09 11:26:05 +1000

    Add files via upload

--- background.js
+++ background.js
@@ -6,7 +6,9 @@ function onInstalled(details) {
 		chrome.storage.sync.set({'color': '[245,245,245]'});
 
 		chrome.tabs.query({}, tabs => {
-			tabs.forEach(tab => chrome.tabs.executeScript(tab.id, { file: 'whitebuster.js'}));
+			tabs.forEach(tab => {
+				try { chrome.tabs.executeScript(tab.id, { file: 'whitebuster.js'}); } catch(err) {};
+			});
 		});
 
 	} else if(details.reason === 'update') {
